9a20bdec444ce1dcb476cccdbda1c537c0112c10
Marta Z Biedes <marta.tzb@gmail.com>
2022-10-22 13:16:19 -0700

    all

---
+++ Backend/controllers/places.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const db = require("../models")
+
+const { Place, Comment, User } = db
+
+router.post('/', async (req, res) => {
+    if (!req.body.pic) {
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    const place = await Place.create(req.body)
+    res.json(place)
+})
+
+
+router.get('/', async (req, res) => {
+    const places = await Place.findAll()
+    res.json(places)
+})
